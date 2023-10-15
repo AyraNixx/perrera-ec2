4e22c3cfc9b4db67ea33f8c88cf1dc7d56f93cab
PaulaMH <paulamorenohermoso99@gmail.com>
2023-10-15 13:38:00 +0200

    Actualizado ruta css

--- public/Login.php
+++ public/Login.php
@@ -7,15 +7,15 @@
 <!DOCTYPE html>
 <html lang="es">
 
-<head>
+<head> 
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" href="/app/views/css/sass.css">
+    <link rel="stylesheet" href="../app/views/css/sass.css">
     <link rel="shortcut icon" href="./imgs/logos/logo1.png" type="image/x-icon">
 </head>
-
+ 
 <body class="bg-white">
 
     <!-- TRIANGULOS DE FONDO -->
